05e21c40648b3cc7f3a277f0552cb8637fff3d4d
Wilfred Hughes <me@wilfred.me.uk>
2014-03-12 22:36:58 +0000

    Serve the hello resource from the index.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var http = require('http');
+var Resource = require('./resource.js');
+
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
+    // TODO: support other content-types.
+    res.writeHead(200, {'Content-Type': 'text/html'});
+
+    Resource.findOne({'path': 'index.html'}, function(err, indexResource) {
+        res.end(indexResource.content);
+    });
 }).listen(1337, '127.0.0.1');
+
 console.log('Server running at http://127.0.0.1:1337/');
 
