ab97efa712e68b3c644015b8ddf09ce69e19d554
Wilfred Hughes <me@wilfred.me.uk>
2014-03-12 23:23:53 +0000

    Expose Resource objects over a JSON API.

--- server.js
+++ server.js
@@ -11,8 +11,20 @@ function index(req, response, next) {
     });
 }
 
+function resource(req, response, next) {
+    Resource.findOne({'path': req.params.path}, function(err, resource) {
+        if (resource === null) {
+            next(new restify.NotFoundError(
+                "No resource with path '" + req.params.path + "'"));
+        } else {
+            response.send(resource)
+        }
+    })
+}
+
 var server = restify.createServer();
 server.get('/', index);
+server.get('/resource/:path', resource);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
