a9475f43feb30a7ca97b03e0b7e17000ec82b0d9
Wilfred Hughes <me@wilfred.me.uk>
2014-10-01 22:53:42 +0100

    Look ma, eval!

--- static/start_editor.js
+++ static/start_editor.js
@@ -1,3 +1,13 @@
-CodeMirror(document.body, {
+/* jshint -W061 */
+var textarea = document.getElementById("editor"),
+    button = document.getElementById("eval-contents");
+
+textarea.value = "console.log(\"hello world!\");";
+
+CodeMirror.fromTextArea(textarea, {
     lineNumbers: true
 });
+
+button.onclick = function() {
+    eval(textarea.value);
+}
