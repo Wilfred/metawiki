1b69eb17f5c9be6fcda27fa211574003d4931ae2
Wilfred Hughes <me@wilfred.me.uk>
2014-10-03 22:51:06 +0100

    Grammar fix.

--- resource.js
+++ resource.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/test');
 
-// A resource a blob of piece of data that can viewed or modified over
+// A resource is a blob of data that can viewed or modified over
 // our REST API.
 // TODO: versioning
 var Resource = mongoose.model('Resource', {
