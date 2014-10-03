56be2dbe41234e9a8a14302f5249e076283ff578
Wilfred Hughes <me@wilfred.me.uk>
2014-10-03 23:22:31 +0100

    Updating server.js to use db.js.

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
 var http = require('http');
-var Resource = require('./resource.js');
+var db = require('./db.js');
 
 var restify = require('restify');
 
 function index(req, response, next) {
     response.setHeader('Content-Type', 'text/html');
-    Resource.findOne({'path': 'index.html'}, function(err, indexResource) {
+    db.Resource.findOne({'path': 'index.html'}, function(err, indexResource) {
         response.writeHead(200);
         response.end(indexResource.content);
     });
 }
 
 function resource(req, response, next) {
-    Resource.findOne({'path': req.params.path}, function(err, resource) {
+    db.Resource.findOne({'path': req.params.path}, function(err, resource) {
         if (resource === null) {
             next(new restify.NotFoundError(
                 "No resource with path '" + req.params.path + "'"));
