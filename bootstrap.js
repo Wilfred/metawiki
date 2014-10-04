0523c0ce568a87d10e3feb19f2a8735a7513ad51
Wilfred Hughes <me@wilfred.me.uk>
2014-10-05 00:01:09 +0100

    Use async.parallel for bootstrapping multiple files.

--- bootstrap.js
+++ bootstrap.js
@@ -1,15 +1,24 @@
 var fs = require("fs");
+var async = require('async');
 var db = require('./db.js');
 
 db.connect();
 
-db.Resource.findOneAndUpdate({
-    category: 'template',
-    path: 'index.html',
-}, {
-    content: fs.readFileSync('index.html', {encoding: 'utf8'})
-}, {
-    upsert: true
-}, function(x) {
+function createResource(resourcePath, localPath, cb) {
+    db.Resource.findOneAndUpdate({
+        category: 'template',
+        path: resourcePath
+    }, {
+        content: fs.readFileSync(localPath, {encoding: 'utf8'})
+    }, {
+        upsert: true
+    }, cb);
+}
+
+async.parallel([
+    function(cb) {
+        createResource("index.html", "index.html", cb);
+    },
+], function(err, res) {
     db.disconnect();
 });
