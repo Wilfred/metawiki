134af2c44a7c256af06d53bb1ba69d0c79b2db4b
Wilfred Hughes <me@wilfred.me.uk>
2014-10-05 00:03:00 +0100

    Use a higher-order function instead of verbose function wrapping.

--- bootstrap.js
+++ bootstrap.js
@@ -4,21 +4,21 @@ var db = require('./db.js');
 
 db.connect();
 
-function createResource(resourcePath, localPath, cb) {
-    db.Resource.findOneAndUpdate({
-        category: 'template',
-        path: resourcePath
-    }, {
-        content: fs.readFileSync(localPath, {encoding: 'utf8'})
-    }, {
-        upsert: true
-    }, cb);
+function createResource(resourcePath, localPath) {
+    return function(cb) {
+        db.Resource.findOneAndUpdate({
+            category: 'template',
+            path: resourcePath
+        }, {
+            content: fs.readFileSync(localPath, {encoding: 'utf8'})
+        }, {
+            upsert: true
+        }, cb);
+    }
 }
 
 async.parallel([
-    function(cb) {
-        createResource("index.html", "index.html", cb);
-    },
+    createResource("index.html", "index.html")
 ], function(err, res) {
     db.disconnect();
 });
