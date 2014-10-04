703de5616c7e3ddb6c1a35c67e266d63159f2383
Wilfred Hughes <me@wilfred.me.uk>
2014-10-05 00:37:16 +0100

    Bootstrap files should have category set.

--- bootstrap.js
+++ bootstrap.js
@@ -4,10 +4,10 @@ var db = require('./db.js');
 
 db.connect();
 
-function createResource(resourcePath, localPath) {
+function createResource(category, resourcePath, localPath) {
     return function(cb) {
         db.Resource.findOneAndUpdate({
-            category: 'template',
+            category: category,
             path: resourcePath
         }, {
             content: fs.readFileSync(localPath, {encoding: 'utf8'})
@@ -18,17 +18,17 @@ function createResource(resourcePath, localPath) {
 }
 
 async.parallel([
-    createResource("index.html", "index.html"),
+    createResource("html", "index.html", "index.html"),
 
     // TODO: treat css and js as separate types of content.
-    createResource("css/codemirror.css",
+    createResource("css", "codemirror.css",
                    "bower_components/codemirror/lib/codemirror.css"),
-    createResource("js/codemirror/codemirror.js",
+    createResource("js", "codemirror/codemirror.js",
                    "bower_components/codemirror/lib/codemirror.js"),
-    createResource("js/codemirror/javascript.js",
+    createResource("js", "codemirror/javascript.js",
                    "bower_components/codemirror/mode/javascript/javascript.js"),
     
-    createResource("js/wikieval/start_editor.js",
+    createResource("js", "wikieval/start_editor.js",
                    "static/start_editor.js"),
 ], function(err, res) {
     db.disconnect();
