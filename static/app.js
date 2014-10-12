43f623626a09b9a9fdb56759eae9d63bfca6af8f
Wilfred Hughes <me@wilfred.me.uk>
2014-10-12 13:06:11 +0100

    Hooking up jquery to app.js.

--- static/app.js
+++ static/app.js
@@ -1,6 +1,8 @@
 // The clientside wiki app.
+var $content = $("#content");
+
 routie('md/:page', function(page) {
-   alert('page: ' + page) 
+    $content.html('page is: ' + page);
 });
 
 routie('md/Home');
