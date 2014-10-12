a750c489a6f4e752d0d5cc2c7234f30c7c62a256
Wilfred Hughes <me@wilfred.me.uk>
2014-10-12 14:23:17 +0100

    Building a basic editor client-side view.

--- static/Home.md
+++ static/Home.md
@@ -1,3 +1,5 @@
 # WikiEval
 
 WikiEval is an experiment in writing a *truly* editable website.
+
+[Edit this page!](#edit/md/Home)
--- static/app.js
+++ static/app.js
@@ -1,11 +1,19 @@
 // The clientside wiki app.
-var $content = $("#content");
+var $content = $("#content"),
+    editorTemplate = Handlebars.compile($("#editor-template").html());
 
 routie('md/:pageName', function(pageName) {
     $.ajax({url:"/resources/md/" + pageName}).done(function(page) {
         $content.html(marked(page.content));
     });
-    
+});
+
+routie('edit/*', function() {
+    var resourceName = window.location.hash.substr("#edit/".length);
+    $content.html(editorTemplate({
+        content: "some content",
+        resourceName: resourceName
+    }));
 });
 
 routie('md/Home');
