115b9229ba42d930e42f42e3a76fc893289c5762
Wilfred Hughes <me@wilfred.me.uk>
2014-10-12 10:22:08 +0100

    Moving views out to separate file.

--- server.js
+++ server.js
@@ -4,38 +4,7 @@ var log = require('./log');
 var db = require('./db');
 db.connect();
 
-function index(req, response, next) {
-    response.setHeader('Content-Type', 'text/html');
-    db.Resource.findOne({'path': 'index.html'}, function(err, indexResource) {
-        response.writeHead(200);
-        response.end(indexResource.content);
-    });
-}
-
-var CATEGORY_MIME_TYPES = {
-    js: 'application/javascript',
-    html: 'text/html',
-    css: 'text/css',
-}
-
-function resource(req, res, next) {
-    var category = req.params[0], path = req.params[1];
-    
-    db.Resource.findOne({
-        'category': category, 'path': path
-    }, function(err, resource) {
-        if (resource === null) {
-            next(new restify.NotFoundError(
-                "No resource with path '" + path + "'"));
-        } else {
-            res.writeHead(200, {
-                'Content-Type': CATEGORY_MIME_TYPES[resource.category] || 'application/octet-stream'
-            });
-            res.write(resource.content);
-            res.end();
-        }
-    });
-}
+var views = require('./views');
 
 var APP_NAME = "WikiEval";
 var server = restify.createServer({
@@ -48,8 +17,8 @@ server.pre(function (request, response, next) {
     next();
 });
 
-server.get('/', index);
-server.get(/^\/resources\/(.+?)\/(.+)$/, resource);
+server.get('/', views.index);
+server.get(/^\/resources\/(.+?)\/(.+)$/, views.resource);
 
 server.listen(8080, function() {
     console.log('==> %s server listening at %s', server.name, server.url);
---
+++ views.js
@@ -0,0 +1,40 @@
+var db = require('./db');
+
+
+function index(req, response, next) {
+    response.setHeader('Content-Type', 'text/html');
+    db.Resource.findOne({'path': 'index.html'}, function(err, indexResource) {
+        response.writeHead(200);
+        response.end(indexResource.content);
+    });
+}
+
+var CATEGORY_MIME_TYPES = {
+    js: 'application/javascript',
+    html: 'text/html',
+    css: 'text/css'
+}, DEFAULT_MIME_TYPE = 'application/octet-stream';
+
+function resource(req, res, next) {
+    var category = req.params[0], path = req.params[1];
+    
+    db.Resource.findOne({
+        'category': category, 'path': path
+    }, function(err, resource) {
+        if (resource === null) {
+            next(new restify.NotFoundError(
+                "No resource with path '" + path + "'"));
+        } else {
+            res.writeHead(200, {
+                'Content-Type': CATEGORY_MIME_TYPES[resource.category] || DEFAULT_MIME_TYPE
+            });
+            res.write(resource.content);
+            res.end();
+        }
+    });
+}
+
+module.exports = {
+    resource: resource,
+    index: index
+};
