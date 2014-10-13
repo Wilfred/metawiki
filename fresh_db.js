09d35b737ff7d9ac71526579e65a7b0e7a3637d3
Wilfred Hughes <me@wilfred.me.uk>
2014-10-14 00:04:34 +0100

    Adding uri.js.

--- fresh_db.js
+++ fresh_db.js
@@ -40,6 +40,9 @@ async.series([
             createResource("js", "jquery/jquery.js",
                            "bower_components/jquery/dist/jquery.js"),
             
+            createResource("js", "uri/uri.js",
+                           "bower_components/uri.js/src/URI.js"),
+            
             createResource("js", "marked/marked.js",
                            "bower_components/marked/lib/marked.js"),
             
