0548e931e02406efa12f2ceedaffe61dab09a84c
Wilfred Hughes <me@wilfred.me.uk>
2014-11-29 19:29:54 +0000

    Adding a MIME type dropdown.

--- models.js
+++ models.js
@@ -1,22 +1,11 @@
 var mongoose = require('mongoose');
 
-// TODO: this should be a database value, not hard-coded.
-var MIME_TYPES = [
-    'application/octet-stream',
-    'application/javascript',
-    'text/html',
-    'text/css',
-    'text/plain'
-];
-
-
 // A resource is a blob of data that can viewed or modified over
 // our REST API.
 // TODO: versioning
 // TODO: mimeType will need to be immutable.
 var Resource = mongoose.model('Resource', {
     path: String,
-    
     content: String,
     mimeType: String
 });
