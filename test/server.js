4f69b075826c3effbaeeca73e86c845c384a8a99
Wilfred Hughes <me@wilfred.me.uk>
2014-12-02 00:15:09 +0000

    Skeleton unit tests.

---
+++ test/server.js
@@ -0,0 +1,9 @@
+var expect = require("chai").expect;
+
+describe("GET resource API", function() {
+   describe("basic fetch", function() {
+       it("should be able to fetch items from the DB", function(){
+           expect(1).to.equal(1);
+       });
+   });
+});
