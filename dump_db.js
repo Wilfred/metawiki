60f317183e28f8636818a4575a1b740e162ef600
Wilfred Hughes <me@wilfred.me.uk>
2014-12-21 00:03:15 +0000

    Adding a basic dump_db script.

---
+++ dump_db.js
@@ -0,0 +1,42 @@
+var fs = require('fs')
+var path = require('path')
+var mkdirp = require('mkdirp')
+var async = require('async')
+var db = require('./db.js')
+var models = require('./models.js')
+
+var resourceCount = 0
+
+async.series([
+    db.connect,
+
+    function(cb) {
+        models.Resource.find({}, function(err, resources) {
+            resourceCount = resources.length
+
+            async.map(resources, function(resource, _cb) {
+
+                var localPath = "bootstrap/" + resource.path
+                async.series([
+                    function(__cb) {
+                        mkdirp(path.dirname(localPath), {}, __cb)
+                    },
+                    function(__cb) {
+                        fs.writeFile(localPath, resource.content, __cb)
+                    },
+                ], _cb)
+            }, cb)
+        })
+    },
+    
+    db.disconnect
+    
+], function(err) {
+        if (err) {
+            console.log(['Failed:', err]);
+        } else {
+            console.log(['Wrote files:', resourceCount]);
+        }
+    }
+)
+     
