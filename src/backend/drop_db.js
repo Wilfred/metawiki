c001e27c2e4c26beefe6fd3f43793d1177e75e1a
Wilfred Hughes <me@wilfred.me.uk>
2015-03-19 22:41:01 +0000

    Adding a drop-db command.

---
+++ src/backend/drop_db.js
@@ -0,0 +1,19 @@
+var async = require('async');
+var db = require('./db.js');
+var models = require('./models.js');
+
+async.series([
+    db.connect,
+
+    function(cb) {
+        models.Resource.remove({}, cb);
+    },
+    
+    db.disconnect
+], function(err) {
+    if (err) {
+        console.log(['Failed:', err]);
+    } else {
+        console.log(['Deleted all resources.']);
+    }
+});
