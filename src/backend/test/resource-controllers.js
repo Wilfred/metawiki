ba2d7afcac7f835420fe0370e77497108f08364d
Wilfred Hughes <me@wilfred.me.uk>
2017-03-18 13:09:31 +0000

    [backend] AllResources should include resource IDs

--- src/backend/test/resource-controllers.js
+++ src/backend/test/resource-controllers.js
@@ -123,6 +123,15 @@ describe("Accessing resources", function() {
                     expect(response).to.have.status(200);
                     expect(response.body.length).to.equal(1);
 
+                    var result = _.omitBy(response.body[0], function(v, k) {
+                        return k.startsWith("_")
+                    });
+                    expect(result).to.deep.equal({
+                        path: 'foo',
+                        content: 'foo',
+                        id: 1,
+                    });
+
                     done();
                 });
         });
