91e84035fe014ee84f65852df3649f259699738a
Wilfred Hughes <me@wilfred.me.uk>
2020-04-15 00:20:41 -0700

    Ensure startup script uses DB connect callback

--- src/backend/main.js
+++ src/backend/main.js
@@ -3,10 +3,13 @@
 var server = require("./server");
 var db = require("./db");
 
-// FIXME: this is actually asynchronous.
-db.connect();
-
-var wikiServer = server.create();
-wikiServer.listen(9002, function() {
-  console.log("==> %s server listening at %s", wikiServer.name, wikiServer.url);
+db.connect(function() {
+  var wikiServer = server.create();
+  wikiServer.listen(9002, function() {
+    console.log(
+      "==> %s server listening at %s",
+      wikiServer.name,
+      wikiServer.url
+    );
+  });
 });
